87d94790e89f658aebefc87bd29098d796296584
abdiawali <abdiongithub2019>
2021-10-27 12:37:09 -0500

    add new place and displays it on homepage 'POST'

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.place_list, name='place_list'),
+    
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Place
+from .forms import NewPlaceForm
 
 
 # Create your views here.
 def place_list(request):
+
+    if request.method == 'POST':
+        #create new place
+        form = NewPlaceForm(request.POST)   #creating a form from data in the request.
+        place = form.save() #creating a model object from form
+        if form.is_valid(): #validation against DB constraints
+            place.save()    #saves place to db
+            return redirect('place_list')   #reloads home page.
+
     places = Place.objects.filter(visited=False).order_by('name')
-    return render(request, 'travel_wishlist/wishlist.html', {'places': places})
+    new_place_form = NewPlaceForm() #creats HTML form
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
