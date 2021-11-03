0c70b79a04f20017895ab4b10ef4edeb86039ccf
abdiawali <abdiongithub2019>
2021-11-02 21:22:27 -0500

    added 404 response

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 
@@ -17,3 +17,23 @@ def place_list(request):
     places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm() #creats HTML form
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+def place_was_visited(request, place_pk):
+    if request.method == 'POST':
+        #place = Place.objects.get(pk=place_pk)
+        place = get_object_or_404(Place, pk=place_pk)
+        place.visited = True
+        place.save()
+    
+    return redirect('place_list')
+
+
+def about(request):
+    author = 'abdifatah'
+    about = 'A website to create a list of place to visit'
+    return render(request, 'travel_wishlist/about.html', {'author': author, 'about':about})
